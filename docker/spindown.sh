b41bf72959d72aa422264519b56026e94a09754a
Paul Buetow <git@mx.buetow.org>
2020-12-28 09:57:04 +0000

    add scripts to start multiple dserv containers for development purposes

---
+++ docker/spindown.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+declare -i NUM_INSTANCES=$1
+declare -i BASE_PORT=2222
+
+for (( i=0; i < $NUM_INSTANCES; i++ )); do
+    port=$[ BASE_PORT + i + 1 ]
+	docker stop dserver-serv$i 
+done
