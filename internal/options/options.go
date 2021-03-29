ac08a3efb23f9eb6b55b88f87d5918bbb51abcb8
Paul Buetow <pbuetow@mimecast.com>
2021-03-29 18:01:49 +0100

    add description of Options

--- internal/options/options.go
+++ internal/options/options.go
@@ -1,3 +1,4 @@
 package options
 
+// Options is a map of all options specified by a DTail client to be transferred to the DTail server (e.g. such as grep context control such as -after, -before and -max).
 type Options map[string]string
