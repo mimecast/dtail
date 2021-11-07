4121a7fc9c24384566760708e2ee7a03de0bd484
Paul Buetow <paul@buetow.org>
2021-11-07 11:35:44 +0200

    Can configure num of docker server instances via NUM_INSTANCES

--- docker/spinup.sh
+++ docker/spinup.sh
@@ -2,10 +2,14 @@
 
 declare -i NUM_INSTANCES=$1
 declare -i BASE_PORT=2222
+declare -r SERVERLIST=serverlist.txt
 
 for (( i=0; i < $NUM_INSTANCES; i++ )); do
     port=$[ BASE_PORT + i + 1 ]
     name=dserver-serv$i
     echo Creating $name
     docker run -d --name $name --hostname serv$i -p $port:2222 dserver:develop
+    echo localhost:$port >> $SERVERLIST.tmp
 done
+
+mv $SERVERLIST.tmp $SERVERLIST
