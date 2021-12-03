b1ff5bf140dbf4bdb43352a50d65d392abbd4025
Paul Buetow <pbuetow@mimecast.com>
2021-12-03 09:38:33 +0000

    comment why none logger documentation is empty

--- internal/io/dlog/loggers/none.go
+++ internal/io/dlog/loggers/none.go
@@ -9,13 +9,30 @@ import (
 // don't log anything
 type none struct{}
 
-func (none) Start(ctx context.Context, wg *sync.WaitGroup)               { wg.Done() }
-func (none) Log(now time.Time, message string)                           {}
+func (none) Start(ctx context.Context, wg *sync.WaitGroup) { wg.Done() }
+
+// Log is empty because the none isn't logging but has to statify the interface.
+func (none) Log(now time.Time, message string) {}
+
+// LogWithColora is empty because the none isn't logging but has to statify the interface.
 func (none) LogWithColors(now time.Time, message, coloredMessage string) {}
-func (none) Raw(now time.Time, message string)                           {}
+
+// Raw is empty because the none isn't logging but has to statify the interface.
+func (none) Raw(now time.Time, message string) {}
+
+// RawWithColors is empty because the none isn't logging but has to statify the interface.
 func (none) RawWithColors(now time.Time, message, coloredMessage string) {}
-func (none) Flush()                                                      {}
-func (none) Pause()                                                      {}
-func (none) Resume()                                                     {}
-func (none) Rotate()                                                     {}
-func (none) SupportsColors() bool                                        { return false }
+
+// Flush is empty because the none isn't logging but has to statify the interface.
+func (none) Flush() {}
+
+// Pause is empty because the none isn't logging but has to statify the interface.
+func (none) Pause() {}
+
+// Resume is empty because the none isn't logging but has to statify the interface.
+func (none) Resume() {}
+
+// Rotate is empty because the none isn't logging but has to statify the interface.
+func (none) Rotate() {}
+
+func (none) SupportsColors() bool { return false }
